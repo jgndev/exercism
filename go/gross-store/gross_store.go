5d4964dbc24a3c038cd00957f5475a60823eb23a
Jeremy Novak <olepunchy@gmail.com>
2024-04-14 15:21:46 -0500

    completed exercise

---
+++ go/gross-store/gross_store.go
@@ -0,0 +1,29 @@
+package gross
+
+// Units stores the Gross Store unit measurements.
+func Units() map[string]int {
+    panic("Please implement the Units() function")
+}
+
+// NewBill creates a new bill.
+func NewBill() map[string]int {
+    //panic("Please implement the NewBill() function")
+    return make(map[string]int)
+}
+
+// AddItem adds an item to customer bill.
+func AddItem(bill, units map[string]int, item, unit string) bool {
+    panic("Please implement the AddItem() function")
+}
+
+// RemoveItem removes an item from customer bill.
+func RemoveItem(bill, units map[string]int, item, unit string) bool {
+    panic("Please implement the RemoveItem() function")
+}
+
+// GetItem returns the quantity of an item that the customer has in his/her bill.
+func GetItem(bill map[string]int, item string) (int, bool) {
+    value, exists := bill[item]
+
+    return value, exists
+}
